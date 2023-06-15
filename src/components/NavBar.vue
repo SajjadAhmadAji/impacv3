453452387c0781e779db18ee875e25b40a6de9eb
DESKTOP-R6MLN8J\Unqie <sajjadahmadaji@gmail.com>
2023-06-15 18:47:10 +0500

    logo update

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,7 +6,7 @@ import { RouterLink, RouterView } from 'vue-router'
       <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
         <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
           <RouterLink class="text-smProxima-Nova-Light inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" to="/">
-            <img alt="IMPAC" class="logo" src="@/assets/images/impac-logo.png" width="125" height="125" />
+            <img alt="IMPAC" class="logo" :src="image" width="125" height="125" />
           </RouterLink>
           <button class="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button" v-on:click="toggleNavbar()">
             <i class="fas fa-bars"></i>
@@ -50,11 +50,13 @@ import { RouterLink, RouterView } from 'vue-router'
     </nav>
   </template>
 <script>
+ import image from "@/assets/images/impac-logo.png"
 export default {
   name: "pink-navbar",
   data() {
     return {
-      showMenu: false
+      showMenu: false,
+      image: image
     }
   },
   methods: {
